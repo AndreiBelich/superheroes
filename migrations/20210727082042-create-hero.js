9729aeb046d8a7866be456f09d780c04e213018c
AndreiBelich <balichandrey89@gmail.com>
2021-07-27 11:42:06 +0300

    extend model Hero and migration heroes

--- migrations/20210727082042-create-hero.js
+++ migrations/20210727082042-create-hero.js
@@ -1,39 +1,55 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Heros', {
+    await queryInterface.createTable('heroes', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      nickName: {
-        type: Sequelize.STRING
+      nickname: {
+        field: "nickname",
+        allowNull: false,
+        type: Sequelize.STRING(64)
       },
       realName: {
-        type: Sequelize.STRING
+        field: "real_name",
+        allowNull: false,
+        type: Sequelize.STRING(64)
+      },
+      catchPhrase: {
+        field: "catch_phrase",
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
       },
       originDescription: {
-        type: Sequelize.STRING
+        field: "origin_description",
+        allowNull: false,
+        type: Sequelize.STRING(512)
       },
       superpowers: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       images: {
+        allowNull: true,
         type: Sequelize.INTEGER
       },
       createdAt: {
+        field: "created_at",
         allowNull: false,
         type: Sequelize.DATE
       },
       updatedAt: {
+        field: "updated_at",
         allowNull: false,
         type: Sequelize.DATE
       }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Heros');
+    await queryInterface.dropTable('heroes');
   }
 };
