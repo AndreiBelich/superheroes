0fa83986c6efda01b8df65d21c4bf58fa63e48d8
AndreiBelich <balichandrey89@gmail.com>
2021-07-27 11:21:19 +0300

    creaate model Hero

---
+++ models/hero.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Hero extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Hero.init({
+    nickName: DataTypes.STRING,
+    realName: DataTypes.STRING,
+    originDescription: DataTypes.STRING,
+    superpowers: DataTypes.INTEGER,
+    images: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Hero',
+  });
+  return Hero;
+};
